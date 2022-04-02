c19108bb35ac9474c72e4b9f61c1f77e54849fde
Danita Codes <danitacodes@gmail.com>
2022-04-01 19:09:36 -0700

    variables

---
+++ variablesPractice.js
@@ -0,0 +1,16 @@
+/* Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”). */
+
+let admin
+name = "John";
+
+admin = name;
+alert(admin);
+
+/*Create a variable with the name of our planet. How would you name such a variable?
+Create a variable to store the name of a current visitor to a website. How would you name that variable? */
+
+const ourPlanet = 'Earth'; //const because it cannot be reassigned
+let vistor = 'Danita'; //let because it can be reassigned
